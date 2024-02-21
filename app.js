c454ce4f8d67544e91116403f17453eab3fda118
vovanhai <vovanhai@monetplus.cz>
2024-02-21 10:48:21 +0700

    first init

---
+++ app.js
@@ -0,0 +1,16 @@
+let print = (value) => {
+    console.log(value);
+}
+
+/**
+ * this function return the summary of 2 variables
+ * @param a first number
+ * @param b seconnumber
+ * @returns the sum of two vars
+ */
+
+let foo=(a,b)=>{
+    return a+b;
+}
+
+
